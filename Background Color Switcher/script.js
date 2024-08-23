e07c60601a2fdb2850c42c0a5243e31d187b2af4
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2024-08-23 21:41:56 +0530

    change if to switch statement

--- Background Color Switcher/script.js
+++ Background Color Switcher/script.js
@@ -3,14 +3,19 @@ const body = document.querySelector("body");
 
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
-    if (e.target.id === "grey") {
-      body.style.backgroundColor = "grey";
-    } else if (e.target.id === "white") {
-      body.style.backgroundColor = "white";
-    } else if (e.target.id === "blue") {
-      body.style.backgroundColor = "blue";
-    } else {
-      body.style.backgroundColor = "yellow";
+    switch (e.target.id) {
+      case "grey":
+        body.style.backgroundColor = "grey";
+        break;
+      case "white":
+        body.style.backgroundColor = "white";
+        break;
+      case "blue":
+        body.style.backgroundColor = "blue";
+        break;
+      default:
+        body.style.backgroundColor = "yellow";
+        break;
     }
   });
 });
